71d7fc2f751d08c698d70f9b45c6525744701181
Krab <krabbch@gmail.com>
2014-12-19 18:27:35 +0500

    Change version to 0.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='tinycurl',
-      version='0.2.3',
+      version='0.2.4',
       description='Simple cURL wrapper',
       author='Krab',
       author_email='krabbch@gmail.com',
